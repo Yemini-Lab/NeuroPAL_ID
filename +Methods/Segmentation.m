b75e4f629fc25e2306b3df61a45bf951867204e0
Kevin Rusch (blueneuron.net) <sep271905@protonmail.com>
2022-12-12 08:00:22 -0500

    watershed

--- +Methods/Segmentation.m
+++ +Methods/Segmentation.m
@@ -114,7 +114,83 @@ classdef Segmentation < handle
             obj.unet_model.save_weights(fullfile(obj.paths.unet_weights, 'weights_initial.h5'));
             fprintf("Loaded the 3D U-Net model\n")
         end
+
+        function segment_vol1(self, method)
+            if nargin < 2
+                method = "min_size";
+            end
         
+            self.vol = 1;
+            [image_cell_bg, l_center_coordinates, segmentation_auto, image_gcn, r_coordinates_segment] = ...
+                self._segment(self.vol, method, true);
+            self.segresult.update_results(image_cell_bg, l_center_coordinates, segmentation_auto, image_gcn, r_coordinates_segment);
+            self.r_coordinates_segment_t0 = self.segresult.r_coordinates_segment.copy();
+            use_8_bit = true;
+            if self.segresult.segmentation_auto.max() <= 255
+                use_8_bit = false;
+            end
+        
+            % save the segmented cells of volume #1
+            save_img3(z_siz=self.z_siz, img=self.segresult.segmentation_auto, ...
+                      path=self.paths.auto_segmentation_vol1 + "auto_t%04i_z%04i.tif", use_8_bit=use_8_bit);
+            fprintf("Segmented volume 1 and saved it\n");
+        end
+
+        function [image_cell_bg, l_center_coordinates, segmentation_auto, image_gcn, r_coordinates_segment] = segment(self, vol, method, print_shape)
+            if nargin < 4
+                print_shape = false;
+            end
+        
+            image_raw = read_image_ts(vol, self.paths.raw_image, self.paths.image_name, [0, self.z_siz], 'print_', print_shape);
+        
+            % image_gcn will be used to correct tracking results
+            image_gcn = image_raw.copy() / 65536.0;
+            image_cell_bg = self._predict_cellregions(image_raw, vol);
+            if max(image_cell_bg(:)) <= 0.5
+                error("No cell was detected by 3D U-Net! Try to reduce the noise_level.");
+            end
+        
+            % segment connected cell-like regions using _watershed
+            segmentation_auto = self._watershed(image_cell_bg, method);
+            if max(segmentation_auto(:)) == 0
+                error("No cell was detected by watershed! Try to reduce the min_size.");
+            end
+        
+            % calculate coordinates of the centers of each segmented cell
+            l_center_coordinates = snm.center_of_mass(segmentation_auto > 0, segmentation_auto, 1:segmentation_auto.max() + 1);
+            r_coordinates_segment = self._transform_layer_to_real(l_center_coordinates);
+        end
+
+        function image_cell_bg = predict_cellregions(self, image_raw, vol)
+            % Predict cell regions by 3D U-Net and save it if the prediction has not been cached
+            try
+                image_cell_bg = load(self.paths.unet_cache + "t%04i.npy", '-mat', 'vol');
+            catch
+                image_cell_bg = self.save_unet_regions(image_raw, vol);
+            end
+        end
+
+        function image_cell_bg = save_unet_regions(self, image_raw, vol)
+            % Predict the cell regions by 3D U-Net and cache the prediction
+            % pre-processing: local contrast normalization
+            image_norm = expand_dims(_normalize_image(image_raw, self.noise_level), [1, 5]);
+            % predict cell-like regions using 3D U-net
+            image_cell_bg = unet3_prediction(image_norm, self.unet_model, 'shrink', self.shrink);
+            save(self.paths.unet_cache + "t%04i.npy", '-v7.3', '-mat', 'vol', 'image_cell_bg');
+        end
+
+        function segmentation_auto = watershed(self, image_cell_bg, method)
+            % Segment the cell regions by watershed method
+            [image_watershed2d_wo_border, ~] = watershed_2d(image_cell_bg(1, :, :, :, 1), 'z_range', self.z_siz, 'min_distance', 7);
+            [~, image_watershed3d_wi_border, min_size, cell_num] = watershed_3d(image_watershed2d_wo_border, 'samplingrate', [1, 1, self.z_xy_ratio], 'method', method, 'min_size', self.min_size, 'cell_num', self.cell_num, 'min_distance', 3);
+            [segmentation_auto, fw, inv] = relabel_sequential(image_watershed3d_wi_border);
+            self.min_size = min_size;
+
+            if strcmp(method, "min_size")
+                self.cell_num = cell_num;
+            end
+
+        end
     end
 
     methods (Static)
--- +Methods/Tracker.m
+++ +Methods/Tracker.m
@@ -1,5 +1,76 @@
 classdef Tracker < handle
 
+    % Data and methods for tracking cells
+    %
+    % Properties:
+    %   volume_num: int
+    %       The number of volumes (time points) of the 3D + T image to be tracked
+    %   x_siz: int
+    %   y_siz: int
+    %   z_siz: int
+    %       Size of each 3D image. Obtained from the siz_xyz parameter (tuple) with (x_siz:Height, y_siz:Width, z_siz:Depth)
+    %   z_xy_ratio: float
+    %       The resolution (length per voxels) ratio between the z axis and the x-y plane, used in tracking.
+    %       Does not need to very precise
+    %   z_scaling: int
+    %       An integer (>= 1) for interpolating/smoothing images along z direction.
+    %       z_scaling = 1 makes no interpolation but only smoothing.
+    %   noise_level: float
+    %       A threshold to discriminate noise/artifacts from cell regions, used in preprocess._normalize_image function
+    %   min_size: int
+    %       A threshold of the minimum cell size (unit: voxels) to remove tiny regions that may be non-cell objects,
+    %       used in watershed.watershed_3d function
+    %   beta_tk: float
+    %       Control coherence by a weighted average of movements from neighbouring cells.
+    %       A larger BETA will include more cells and thus generates more coherent movements.
+    %   lambda_tk: float
+    %       Control coherence by applying a penalty for the incoherence of cell movements.
+    %       A large LAMBDA will generates more coherent movements.
+    %   maxiter_tk: int
+    %       The maximum number of iterations of PR-GLS during once application of FFN + PR-GLS.
+    %       A large values will generate more accurate tracking but will also increase the runtime.
+    %   cell_num: int
+    %       The number of cells to be extracted from watershed. It is used only when segmentation method is "cell_num".
+    %       Default: 0
+    %   ensemble: bool or int, optional
+    %       If false, track cells in single mode. If an integer, track cells in ensemble mode.
+    %       The integer indicates number of predictions to be averaged. Default: false
+    %   adjacent: bool, optional
+    %       Only used in ensemble mode. If true, make predictions of cell positions at t from
+    %       adjacent previous volumes such as t-1, t-2, t-3,..., t-10 (when ensemble=10).
+    %       If false, make prediction from distributed previous volumes. For example, predict cells at t=101 from
+    %       t = 1, 11, 21, ..., 91 (when ensemble=10). Default: false
+    %   shrink: tuple, optional
+    %       For padding the images before applying U-Net and for shrinking the cell prediction in the center part
+    %       of the U-Net output. Each value should be < (x, y, or z size of the U-Net input // 2), Default: (24, 24, 2)
+    %   miss_frame: list, optional
+    %       A list of volumes (int) which includes serious problem in the raw images and thus will be skipped for tracking.
+    %       Default: none
+    %   cell_num_t0: int
+    %       The detected cell numbers in the manually corrected segmentation in volume 1
+    %   Z_RANGE_INTERP: Range object
+    %       The sequence of the indexes along z axis in the interpolated image corresponding to the layers in the raw image
+    %   region_list: list
+    %       List of the 3D sub-images [array_cell1, array_cell2, ...] with binary values
+    %       (1: this cell; 0: background or other cells)
+    %   region_width: list
+    %       List of the width of [[x_width, y_width, z_width]_cell1, ...] each sub-image in x, y, and z axis
+    %   region_xyz_min: list
+    %       List of the minimum coordinates [[x_min, y_min, z_min]_cell1, ...] of each sub-image in the raw image
+    %   pad_x: int
+    %   pad_y: int
+    %   pad_z: int
+    %       The values for padding a zero array with the raw image size
+    %   label_padding: numpy.ndarray
+    %       A 3D array with zero values, used in accurate correction during tracking.
+    %       Shape: (row + 2 * pad_x, column + 2 * pad_y, layer + 2 * pad_z)
+    %   segmentation_manual_relabels: numpy.ndarray
+    %       The relabeled manual segmentation. Shape: (row, column, layer)
+    %   segresult: SegResults object
+    %       The results of the segmentation in current volume
+    %   unet_model: keras.Model
+    %       The pretrained/retrained 3D U-Net model
+
     properties % Public Access
         miss_frame
         noise_level
@@ -172,6 +243,93 @@ classdef Tracker < handle
                 error("step should be an interger >= 0")
             end
         end
+
+        function interpolate_seg(self)
+            % _interpolate layers in z axis
+            self.seg_cells_interpolated_corrected = self._interpolate();
+            self.Z_RANGE_INTERP = self.z_scaling // 2:self.z_scaling:self.seg_cells_interpolated_corrected.shape(3);
+        
+            % re-segmentation
+            self.seg_cells_interpolated_corrected = self._relabel_separated_cells(self.seg_cells_interpolated_corrected);
+            self.segmentation_manual_relabels = self.seg_cells_interpolated_corrected(:,:,self.Z_RANGE_INTERP);
+        
+            % save labels in the first volume (interpolated)
+            save_img3ts(0:self.z_siz-1, self.segmentation_manual_relabels, self.paths.track_results+"track_results_t%04i_z%04i.tif", 1, self.use_8_bit);
+        
+            % calculate coordinates of cell centers at t=1
+            center_points_t0 = snm.center_of_mass(self.segmentation_manual_relabels > 0, self.segmentation_manual_relabels, 1:self.segmentation_manual_relabels.max());
+            r_coordinates_manual_vol1 = self._transform_layer_to_real(center_points_t0);
+            self.r_coordinates_tracked_t0 = r_coordinates_manual_vol1;
+            self.cell_num_t0 = size(r_coordinates_manual_vol1, 1);
+        end
+
+        function seg_cells_interpolated_corrected = self.interpolate()
+            seg_cells_interpolated = gaussian_filter(self.segmentation_manual_relabels, z_scaling=self.z_scaling, smooth_sigma=2.5);
+            seg_cell_or_bg = zeros(size(self.segmentation_manual_relabels));
+            seg_cells_interpolated_corrected = watershed_2d_markers(seg_cells_interpolated, seg_cell_or_bg, z_range=self.z_siz * self.z_scaling + 10);
+            seg_cells_interpolated_corrected = seg_cells_interpolated_corrected(5:self.x_siz + 5, 5:self.y_siz + 5, 5:self.z_siz * self.z_scaling + 5);
+        end
+
+        function cal_subregions(self)
+            % Compute subregions of each cells for quick "accurate correction"
+            seg_16 = int16(self.seg_cells_interpolated_corrected);
+        
+            [self.region_list, self.region_width, self.region_xyz_min] = get_subregions(seg_16, max(seg_16(:)));
+            self.pad_x = max(self.region_width(:, 1));
+            self.pad_y = max(self.region_width(:, 2));
+            self.pad_z = max(self.region_width(:, 3));
+            self.label_padding = padarray(seg_16, [self.pad_x, self.pad_y, self.pad_z], 0, 'both');
+        end
+        
+        function check_multicells(self)
+            for i = 1:numel(self.region_list)
+                region = self.region_list{i};
+                assert sum(unique(label(region))) == 1, sprintf('more than one cell in region %d', i);
+            end
+        end
+
+        function load_ffn(self)
+            self.ffn_model = load_model(fullfile(self.paths.models, self.paths.ffn_model_file));
+            fprintf('Loaded the FFN model\n');
+        end
+
+
+        function should_stop = evaluate_correction(self, r_displacement_correction)
+            % evaluate if the accurate correction should be stopped
+            i_disp_test = r_displacement_correction;
+            i_disp_test(:, 3) = i_disp_test(:, 3) .* self.z_scaling / self.z_xy_ratio;
+            if max(abs(i_disp_test(:))) >= 0.5
+                % fprintf('%f,', max(abs(i_disp_test(:))));
+                should_stop = false;
+            else
+                % fprintf('%f\n', max(abs(i_disp_test(:))));
+                should_stop = true;
+            end
+        end
+
+        function tracked_labels = transform_motion_to_image(self, cells_on_boundary_local, i_disp_from_vol1_updated)
+            % Transform the predicted movements to the moved labels in 3D image
+            [i_tracked_cells_corrected, i_overlap_corrected] = self._transform_cells_quick(i_disp_from_vol1_updated);
+            % re-calculate boundaries by _watershed
+            i_tracked_cells_corrected(i_overlap_corrected > 1) = 0;
+            for i = cells_on_boundary_local == 1
+                i_tracked_cells_corrected(i_tracked_cells_corrected == i) = 0;
+            end
+            tracked_labels = watershed_2d_markers(i_tracked_cells_corrected(:,:,self.Z_RANGE_INTERP), i_overlap_corrected(:,:,self.Z_RANGE_INTERP), z_range=self.z_siz);
+        end
+
+        function save_coordinates(self)
+            coord = self.history.r_tracked_coordinates;
+            coord = cell2mat(coord);
+            t = size(coord, 1);
+            cell = size(coord, 2);
+            pos = size(coord, 3);
+            coord_table = [repmat(1:t, cell, 1), repmat((1:cell)', t, 1), reshape(coord, t * cell, pos)];
+            dlmwrite(fullfile(self.paths.track_information, 'tracked_coordinates.csv'), coord_table, 'delimiter', ',', 'precision', 6);
+            fprintf('Cell coordinates were stored in ./track_information/tracked_coordinates.csv\n');
+        end
+
+
     end
 end
 
