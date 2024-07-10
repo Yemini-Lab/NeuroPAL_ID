30ce539c78e5e05344f57e2f37bdd4ebe09416f8
Kevin Rusch (LabPC) <kwrusch@seas.upenn.edu>
2024-07-10 12:28:40 -0400

    TrackMate XML write debugging

--- +DataHandling/readTrackmate.m
+++ +DataHandling/readTrackmate.m
@@ -1,4 +1,4 @@
-function [n_arr, l_arr] = readTrackmate(file)
+function [n_arr, l_arr, t_gap] = readTrackmate(file)
     xml_contents = xml2struct(file);
     xml_model = xml_contents.TrackMate.Model;
     neuron_rois = xml_model.AllSpots;
@@ -7,24 +7,33 @@ function [n_arr, l_arr] = readTrackmate(file)
 
     n_arr = zeros([1 4]);
     l_arr = {};
+    t_gap = 0;
 
     for t=1:nt
-        target = neuron_rois.SpotsInFrame{t};
-        for n=1:size(target.Spot, 2)
-            x = str2num(target.Spot{n}.Attributes.POSITION_X);
-            y = str2num(target.Spot{n}.Attributes.POSITION_Y);
-            z = str2num(target.Spot{n}.Attributes.POSITION_Z);
-            label = target.Spot{n}.Attributes.name;
-            frame = str2num(target.Spot{n}.Attributes.POSITION_T);
-
-            if ~any(isempty([x y z]))
-                n_arr = [n_arr; [frame x y z]];
-                l_arr{end+1} = label;
+        try
+            target = neuron_rois.SpotsInFrame{t};
+            for n=1:size(target.Spot, 2)
+                x = str2num(target.Spot{n}.Attributes.POSITION_X);
+                y = str2num(target.Spot{n}.Attributes.POSITION_Y);
+                z = str2num(target.Spot{n}.Attributes.POSITION_Z);
+                label = target.Spot{n}.Attributes.name;
+                frame = str2num(target.Spot{n}.Attributes.POSITION_T);
+    
+                if ~any(isempty([x y z]))
+                    n_arr = [n_arr; [frame x y z]];
+                    l_arr{end+1} = label;
+                end
             end
+        catch
+            % ...
         end
     end
 
     if min(n_arr(:, 1))==0
+        t_gap = 1;
         n_arr(:, 1) = n_arr(:, 1) + 1;
+    elseif min(n_arr(:, 1))==2
+        t_gap = -1;
+        n_arr(:, 1) = n_arr(:, 1) - 1;
     end
 end
--- +DataHandling/writeTrackMate.m
+++ +DataHandling/writeTrackMate.m
@@ -1,4 +1,4 @@
-function writeTrackMate(video_info, video_neurons, output_file, figure)
+function writeTrackMate(t_gap, video_info, video_neurons, output_file, figure)
     template_file = 'Data\NeuroPAL\xmlTemplate.xml';
 
     fileID = fopen(template_file, 'r');
@@ -11,9 +11,9 @@ function writeTrackMate(video_info, video_neurons, output_file, figure)
     plaintext = splitlines(fileread(template_file));
     new_file = plaintext(1:lineIndex);
     
-    sif_start = '<SpotsInFrame frame="%.f">';
-    spot_line = '\n <Spot ID="%.f" name="%s" STD_INTENSITY_CH1="0" STD_INTENSITY_CH2="0" STD_INTENSITY_CH3="0" QUALITY="0" TOTAL_INTENSITY_CH3="0" POSITION_T="%.f" TOTAL_INTENSITY_CH2="0" TOTAL_INTENSITY_CH1="0" CONTRAST_CH1="0" FRAME="%.f" MEAN_INTENSITY_CH3="0" CONTRAST_CH3="0" CONTRAST_CH2="0" MEAN_INTENSITY_CH1="0" MAX_INTENSITY_CH2="0" MEAN_INTENSITY_CH2="0" MAX_INTENSITY_CH3="0" MAX_INTENSITY_CH1="0" MIN_INTENSITY_CH3="0" MIN_INTENSITY_CH2="0" MIN_INTENSITY_CH1="0" SNR_CH3="0" SNR_CH1="0" SNR_CH2="0" MEDIAN_INTENSITY_CH1="0" VISIBILITY="1" RADIUS="6.0" MEDIAN_INTENSITY_CH2="0" MEDIAN_INTENSITY_CH3="0" POSITION_X="%f" POSITION_Y="%f" POSITION_Z="%.f" />';
-    sif_end = sprintf('\n </SpotsInFrame>');
+    sif_start = '          <SpotsInFrame frame="%.f">';
+    spot_line = '\n            <Spot ID="%.f" name="%s" STD_INTENSITY_CH1="0" STD_INTENSITY_CH2="0" STD_INTENSITY_CH3="0" QUALITY="0" TOTAL_INTENSITY_CH3="0" POSITION_T="%.f" TOTAL_INTENSITY_CH2="0" TOTAL_INTENSITY_CH1="0" CONTRAST_CH1="0" FRAME="%.f" MEAN_INTENSITY_CH3="0" CONTRAST_CH3="0" CONTRAST_CH2="0" MEAN_INTENSITY_CH1="0" MAX_INTENSITY_CH2="0" MEAN_INTENSITY_CH2="0" MAX_INTENSITY_CH3="0" MAX_INTENSITY_CH1="0" MIN_INTENSITY_CH3="0" MIN_INTENSITY_CH2="0" MIN_INTENSITY_CH1="0" SNR_CH3="0" SNR_CH1="0" SNR_CH2="0" MEDIAN_INTENSITY_CH1="0" VISIBILITY="1" RADIUS="6.0" MEDIAN_INTENSITY_CH2="0" MEDIAN_INTENSITY_CH3="0" POSITION_X="%f" POSITION_Y="%f" POSITION_Z="%.f" />';
+    sif_end = sprintf('\n            </SpotsInFrame>');
 
     idx = 0;
     nn = max(size(video_neurons));
@@ -23,40 +23,37 @@ function writeTrackMate(video_info, video_neurons, output_file, figure)
     end
 
     for t=1:video_info.nt
-        t = t-1;
-
         if exist('d', 'var')
             d.Value = t/video_info.nt;
         end
 
-        new_frame = sprintf(sif_start, t);
+        new_frame = sprintf(sif_start, t+t_gap);
     
         for n=1:nn
             if exist('d', 'var')
                 d.Value = min((t+n/nn)/video_info.nt, 1);
             end
 
-            name = video_neurons(n).worldline.name;
-    
             try
-                x = video_neurons(n).rois(t).x_slice;
-                y = video_neurons(n).rois(t).y_slice;
-                z = video_neurons(n).rois(t).z_slice;
+                name = video_neurons(n).worldline.name;
+        
+                x = video_neurons(n).rois(t+t_gap).x_slice;
+                y = video_neurons(n).rois(t+t_gap).y_slice;
+                z = video_neurons(n).rois(t+t_gap).z_slice;
+    
+                new_frame = [new_frame, sprintf(spot_line, idx, name, t+t_gap, t+t_gap, x, y, z)];
+                idx = idx + 1;
             catch
-                x = "";
-                y = "";
-                z = "";
+                % ...
             end
-
-            new_frame = [new_frame, sprintf(spot_line, idx, name, t, t, x, y, z)];
-            idx = idx + 1;
         end
 
-        new_file{end+1} = [new_frame, sif_end];
+        if ~strcmp(new_frame, sprintf(sif_start, t+t_gap))
+            new_file{end+1} = [new_frame, sif_end];
+        end
     end
     
-    new_file{lineIndex} = sprintf('        <AllSpots nspots="%.f"> \n', idx);
-    new_file{end+1} = sprintf('</AllSpots> \n');
+    new_file{lineIndex} = sprintf('        <AllSpots nspots="%.f">', idx);
     new_file = [new_file; plaintext(lineIndex+1:end)];
     writecell(new_file, output_file, FileType='text', QuoteStrings='none');
 
