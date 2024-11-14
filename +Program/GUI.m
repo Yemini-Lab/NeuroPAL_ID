3c45555dff91fedab2a7d9008d4c60afb3851204
Kevin Rusch <SEP271905@protonmail.com>
2024-11-14 16:34:54 -0500

    refactor of gui funcs to quickset values

--- +Program/GUI.m
+++ +Program/GUI.m
@@ -1,6 +1,8 @@
 classdef GUI
     
-    properties
+    properties (Constant)
+        cursor_color = '#9c9c9c';
+        cursor_width = 0.2;
     end
     
     methods (Static)
@@ -23,6 +25,86 @@ classdef GUI
             end
             
             handle = window_handle;
+        end
+
+        function set_limits(varargin)
+            
+            p = inputParser;
+            addOptional(p, 'nx', 'state');
+            addOptional(p, 'ny', []);
+            addOptional(p, 'nz', package);
+            addOptional(p, 'nt', package);
+
+            parse(p, varargin{:});
+
+            nx = p.Results.nx;
+            ny = p.Results.ny;
+            nz = p.Results.nz;
+            nt = p.Results.nt;
+
+            app = Program.GUI.app;
+            app.proc_xSlider.Limits = [1 nx];
+            app.proc_ySlider.Limits = [1 ny];
+            app.proc_zSlider.Limits = [1 nz];
+            app.proc_hor_zSlider.Limits = [1 nz];
+            app.proc_vert_zSlider.Limits = [1 nz];
+
+            if app.proc_xSlider.Value ~= 1 || app.proc_ySlider.Value ~= 1 || app.proc_zSlider.Value ~= 1 || app.proc_tSlider.Value ~= 1   
+                Program.GUI.initialize_values();
+            end
+        end
+
+        function initialize_values()
+            app = Program.GUI.app;
+
+            app.proc_xSlider.Value = round(app.proc_xSlider.Limits(2)/2);
+            app.proc_ySlider.Value = round(app.proc_ySlider.Limits(2)/2);
+
+            app.proc_zSlider.Value = round(app.proc_zSlider.Limits(2)/2);
+            app.proc_vert_zSlider.Value = app.proc_zSlider.Value;
+            app.proc_hor_zSlider.Value = app.proc_zSlider.Value;
+
+            app.proc_tSlider.Value = 1;
+
+            app.proc_zEditField.Value = app.proc_zSlider.Value;
+            app.proc_tEditField.Value = app.proc_tSlider.Value;
+            
+            app.ProcZSlicesEditField.Value = app.proc_zSlider.Value;
+        end
+
+        function indices(channel, idx)
+            app = Program.GUI.app;
+        end
+
+        function gammas(gamma, idx)
+            app = Program.GUI.app;
+
+            if ~exist('idx', 'var')
+                for idx = 1:Program.Handlers.channels.max_nc
+                    Program.GUI.gammas(gamma(idx), idx);
+                end
+
+                return
+            end
+
+            component_name = sprintf("%s_GammaEditField", Program.Handlers.handles.ch_pfx{idx});
+            app.(component_name).Value = gamma;
+        end
+
+        function toggle_loading(handle)
+            if nargin > 0
+                handle = Program.GUI.window;
+            end
+
+            if ~Program.states.instance().is_loading
+                uiimage( ...
+                    "ImageSource", fullfile('Data', 'Images', 'loading.gif'), ...
+                    "ScaleMethod", 'fit', ...
+                    'Position', handle.Position);
+                drawnow;
+            end
+
+            Program.states.toggle('is_loading');
         end
     end
 end
