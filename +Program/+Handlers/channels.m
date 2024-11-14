c1b81dbf7a28b6c25cc2b1347dc992aa7c3179ec
Kevin Rusch <SEP271905@protonmail.com>
2024-11-14 13:46:11 -0500

    Create channels.m

---
+++ +Program/+Handlers/channels.m
@@ -0,0 +1,269 @@
+classdef channels
+    %CHANNEL Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Constant)
+        channel_options = {'Red', 'Green', 'Blue', 'White', 'DIC', 'GFP', 'N/A'};
+
+        up_signs = {"-1", "up", "⮝"};
+        down_signs = {"1", "down", "⮟"};
+
+        dd_string = "proc_c%.f_dropdown";
+        cb_string = "proc_c%.f_checkbox";
+        rep_string = "proc_c%.f_rep";
+        grid_string = "EditChannelsGrid";
+        button_string = "EditChannelsButton";
+
+        label_colors = {'#000', '#000', '#fff', '#000', '#fff', '#000'};
+        channel_colors = {'#ff0000', '#00d100', '#0000ff', '#fff', '#6b6b6b', '#ffff00'};
+
+        fluorophore_mapping = dictionary( ...
+            'red', {{'neptune', 'nep', 'n2.5', 'n25'}}, ...
+            'green', {{'cyofp1', 'cyofp', 'cyo'}}, ...
+            'blue', {'bfp'}, ...
+            'white', {{'rfp', 'tagrfp', 'tagrfp1'}}, ...
+            'dic', {{'dic', 'dia', 'nomarski', 'phase'}}, ...
+            'gfp', {{'gfp', 'gcamp'}});
+    end
+    
+    methods (Static, Access = public)
+        function initialize(input)
+            app = Program.GUI.app;
+            [ch_grid, ~] = Program.Handlers.channels.get_gui(app);
+            n_rows = length(ch_grid.RowHeight);
+
+            if isfile(input) || ischar(input) || isstring(input)
+                [nc, names] = Program.Handlers.channels.channels_from_file(input);
+            else
+                names = input;
+                nc = length(names);
+            end
+
+            [names, ~] = Program.Handlers.channels.autosort(names);
+
+            % Create any missing grid rows.
+            for row=n_rows:nc
+                Program.Handlers.channels.create_channel(app, ch_grid);
+            end
+
+            % Delete any excess grid rows.
+            for row=n_rows:-1:nc+1
+                Program.Handlers.channels.delete_channel(app, row, 1, 0);
+            end
+
+            % Populate gui components.
+            for c=1:nc
+                if isprop(app, sprintf(Program.Handlers.channels.dd_string, c))
+                    dd = app.(sprintf(Program.Handlers.channels.dd_string, c));
+                else
+                    target = Program.Handlers.channels.get_channel(app, c);
+                    dd = target.gui.dd;
+                end
+
+                dd.Items = names;
+                dd.Value = names{c};
+            end
+            
+            % Style non-rgb gui components.
+            non_rgb = keys(Program.Handlers.channels.fluorophore_mapping);
+            non_rgb = non_rgb(4:end);
+            for c=1:length(non_rgb)
+                component = app.(sprintf(Program.Handlers.channels.rep_string, c+3));
+
+                if ~isgraphics(component)
+                    continue
+                end
+
+                for item=1:length(component.Items)
+                    style_idx = item+3;
+                    rep_style = uistyle( ...
+                        "FontColor", Program.Handlers.channels.label_colors{style_idx}, ...
+                        "FontWeight", "bold", ...
+                        "BackgroundColor", Program.Handlers.channels.channel_colors{style_idx});
+                    addStyle(component, rep_style, "item", item);
+
+                end
+            end
+        end
+
+        function handles = create_channel()
+            [ch_grid, ~] = Program.Handlers.channels.get_gui();
+            ch_grid.RowHeight(end+1) = ch_grid.RowHeight(end);
+
+            handles = struct( ...
+                'cb', {Program.GUI.channel.create_checkbox}, ...
+                'dd', {Program.GUI.channel.create_dropdown}, ...
+                'label', {Program.GUI.channel.create_label}, ...
+                'move_up', {Program.GUI.channel.create_button('up')}, ...
+                'move_down', {Program.GUI.channel.create_button('down')}, ...
+                'delete', {Program.GUI.channel.create_button('delete')});
+        end
+
+        function update_channel(event)
+            app = Program.GUIHandling.app;
+
+            newValue = event.Value;
+            oldValue = event.PreviousValue;
+            
+            dropdowns = [];
+            switch event.Source.Layout.Column
+                case 3
+                    for dd=4:length(event.Source.Items)+3
+                        component = app.(sprintf(Program.Handlers.channels.rep_string, dd));
+                        if isgraphics(component)
+                            dropdowns = [dropdowns, component];
+                        end
+                    end
+                    
+                case 4
+                    for dd=1:length(event.Source.Parent.RowHeight)
+                        target = Program.Handlers.channels.get_channel(app, dd);
+                        dropdowns = [dropdowns, target.gui.dd];
+                    end
+            end
+            
+            otherDropdowns = setdiff(dropdowns, event.Source);
+            for k = 1:length(otherDropdowns)
+                if strcmp(otherDropdowns(k).Value, newValue)
+                    otherDropdowns(k).Value = oldValue;
+                    break;
+                end
+            end
+        end
+
+        function channel = get_channel(target)
+            app = Program.GUIHandling.app;
+            dd_name = sprintf(Program.Handlers.channels.dd_string, target);
+            cb_name = sprintf(Program.Handlers.channels.cb_string, target);
+            components = struct('dd', {{}}, 'cb', {{}});
+
+            if isprop(app, dd_name)
+                components.dd = app.(dd_name);
+                components.cb = app.(cb_name);
+    
+                channel = struct( ...
+                    'idx', target, ...
+                    'gui', {components}, ...
+                    'cached_values', {struct('dd', {components.dd.Value}, 'cb', {components.cb.Value})});
+            else
+                [ch_grid, ~] = Program.Handlers.channels.get_gui(app);
+                for n=1:length(ch_grid.Children)
+                    child = ch_grid.Children(n);
+                    if child.Layout.Row==target && isa(child, 'matlab.ui.control.CheckBox')
+                        components.cb = child;
+                    elseif child.Layout.Row==target && isa(child, 'matlab.ui.control.DropDown')
+                        components.dd = child;
+                    end
+                end
+    
+                channel = struct( ...
+                    'idx', target, ...
+                    'gui', {components}, ...
+                    'cached_value', {struct('dd', {components.dd.Value}, 'cb', {components.cb.Value})});
+            end
+        end
+        
+        function move_channel(channel, direction)
+            direction = -1+2*(any(ismember(direction, [Program.Handlers.channels.down_signs{:}])));
+            target = Program.Handlers.channels.get_channel(channel);
+
+            if Program.Handlers.channels.can_move(target.gui.dd, direction)
+                neighbor = Program.Handlers.channels.get_channel(channel + direction);
+
+                target.gui.dd.Value = neighbor.cached_values.dd;
+                neighbor.gui.dd.Value = target.cached_values.dd;
+
+                target.gui.cb.Value = neighbor.cached_values.cb;
+                neighbor.gui.cb.Value = target.cached_values.cb;
+            end
+
+            %Program.GUIHandling.histogram_handler(app, 'draw');
+        end
+
+        function delete_channel(app, channel, permanence_flag, from_file)
+            target = Program.Handlers.channels.get_channel(app, channel);
+
+            if permanence_flag
+                grid = target.gui.dd.Parent;
+
+                for n=numel(grid.Children):-1:1
+                    child = grid.Children(n);
+                    if child.Layout.Row == channel
+                        delete(child)
+                    end
+                end
+
+                temp_rows = grid.RowHeight;
+                temp_rows(channel) = [];
+                grid.RowHeight = temp_rows;
+            else
+                %target.gui.dd.Value = "N/A";
+                target.gui.dd.Enable = "off";
+    
+                target.gui.cb.Value = 0;
+                target.gui.cb.Enable = "off";
+
+                app.(sprintf(Program.Handlers.channels.up_string, channel)).Enable = "off";
+                app.(sprintf(Program.Handlers.channels.down_string, channel)).Enable = "off";
+                app.(sprintf(Program.Handlers.channels.del_string, channel)).Enable = "off";
+            end
+
+            if from_file
+                Methods.ChunkyMethods.delete_channel(channel);
+            end
+
+            %Program.GUIHandling.histogram_handler(app, 'draw');
+        end
+    end
+
+    methods (Static, Access = private)
+        function [grid, edit_button] = get_gui()
+            app = Program.GUIHandling.app;
+            grid = app.(Program.Handlers.channels.grid_string);
+            edit_button = app.(Program.Handlers.channels.button_string);
+        end
+
+        function has_space = can_move(channel, direction)
+            switch num2str(direction)
+                case Program.Handlers.channels.up_signs
+                    has_space = channel.Layout.Row > 1;
+                case Program.Handlers.channels.down_signs
+                    has_space = channel.Layout.Row < length(channel.Parent.RowHeight);
+            end
+        end
+
+        function handle = create_checkbox()
+            handle = uicheckbox(ch_grid);
+            handle.Text = '';
+            handle.Layout.Row = length(ch_grid.RowHeight);
+            handle.Layout.Column = 1;
+        end
+
+        function handle = create_dropdown()
+            handle = uidropdown(ch_grid);
+            handle.Items = Program.GUI.channel.channel_options;
+            handle.Value = 'Red';
+            handle.Layout.Column = [3 4];
+        end
+
+        function handle = create_label()
+            handle = uilabel(ch_grid);
+            handle.BackgroundColor = [0.902 0.902 0.902];
+            handle.HorizontalAlignment = 'center';
+            handle.FontWeight = 'bold';
+            handle.Text = num2str(handles.cb.Layout.Row);
+        end
+
+        function handle = create_button(label)
+            handle = uibutton(ch_grid, 'push');
+            handle.ButtonPushedFcn = @app.proc_c1_move_upPushed2;
+            handle.Text = label;
+
+            if strcmp(label, '🗑')
+                handle.BackgroundColor = [1 0.2784 0.2784];
+                handle.FontWeight = 'bold';
+            end
+        end
+    end
+end
+
