ab38bfffbcc97c6eff4026a14f7dbe23b7891330
Kevin Rusch <SEP271905@protonmail.com>
2024-11-20 11:16:22 -0500

    minor refactor of active_volume

--- +Program/+Handlers/active_volume.m
+++ +Program/+Handlers/active_volume.m
@@ -3,11 +3,11 @@ classdef active_volume
     end
     
     methods (Static)
-        function obj = all()
+        function obj = all()            
             volume_type = Program.Handlers.active_volume.type;
             volume_array = Program.Handlers.active_volume.array;
             volume_dims = Program.Handlers.active_volume.dims;
-            volume_channels = Program.Handlers.channels.indices;
+            volume_channels = Program.Handlers.channels.read_gui;
 
             arr_dims = size(volume_array);
             if volume_dims(1:length(arr_dims)) ~= arr_dims
@@ -20,8 +20,31 @@ classdef active_volume
                 'array', {volume_array}, ...
                 'channels', {volume_channels});
         end
+        
+        function volume_type = type()
+            % Return string describing current volume type (video/colormap)
+
+            if Program.states.instance().is_video
+                volume_type = 'video';
+            else
+                volume_type = 'colormap';
+            end
+        end
+
+        function volume_dims = dims()
+            % Return (x, y, z, c, t) array describing volume dimensions.
+
+            volume_dims = [ ...
+                DataHandling.file.metadata.nx ...
+                DataHandling.file.metadata.ny ...
+                DataHandling.file.metadata.nz ...
+                DataHandling.file.metadata.nc ...
+                DataHandling.file.metadata.nt];
+        end
 
         function volume_array = array()
+            % Return array representing currently indexed array.
+
             if Program.states.instance().is_lazy
                 z = DataHandling.file.metadata.nz;
                 c = DataHandling.channels.indices.lazy_load;
@@ -58,19 +81,6 @@ classdef active_volume
                     volume_array = cat(dim, volume_array(:, :, :, 1:c-1), blank_channel, volume_array(:, :, :, c:end));
                 end
             end
-        end
-        
-        function volume_type = type()
-            volume_type = lower(Program.GUIHandling.volume_type);
-        end
-
-        function volume_dims = dims()
-            volume_dims = [ ...
-                DataHandling.file.metadata.nx ...
-                DataHandling.file.metadata.ny ...
-                DataHandling.file.metadata.nz ...
-                DataHandling.file.metadata.nc ...
-                DataHandling.file.metadata.nt];
         end
     end
 end
