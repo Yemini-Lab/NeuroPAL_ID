860534b330de3289f808f4dc23b9be855051d7b0
Kevin Rusch <SEP271905@protonmail.com>
2024-11-22 13:50:47 -0500

    added parse_scale func to nd2 helper

--- +DataHandling/+Helpers/nd2.m
+++ +DataHandling/+Helpers/nd2.m
@@ -3,8 +3,8 @@ classdef nd2
     
     properties (Access = public, Constant)
         key_map = dictionary( ...
-            'xy_scale', {'Global dCalibration'}, ...
-            'z_scale', {'Global dZStep'});
+            'xy_scale', {'dCalibration'}, ...
+            'z_scale', {'dZStep'});
 
         channel_keys = {'ChannelName', 'ChannelOrder', 'ChannelCount', 'ChannelColor'}; % Keys used to identify channel metadata
         name_substr = 'Global Name #'; % Base substring used to identify channel names globally
@@ -98,132 +98,6 @@ classdef nd2
             save(np_file, 'version', 'data', 'info', 'prefs', 'worm', '-v7.3');
 
             DataHandling.Helpers.nd2.write_data(np_file, file);
-        end
-
-        function [image, metadata] = legacy_read(file)
-            %IMREADND2 Read in a Nikon ND2 image.
-            %
-            %   [IMAGE, METADATA] = IMREADCZI(FILENAME)
-            %
-            %   Input:
-            %   filename - the filename of the ND2 image
-            %
-            %   Outputs:
-            %   image - the image, a struct with fields:
-            %       pixels     = the number of pixels as (x,y,z)
-            %       scale      = the pixel scale, in meters, as (x,y,z)
-            %       channels   = the names of the channels
-            %       colors     = the color for each channel as (R,G,B)
-            %       dicChannel = the DIC channel number
-            %       lasers     = the laser wavelength for each channel
-            %       emissions  = the emssion band for each channel as (min,max)
-            %       data       = the image data as (x,y,z,channel)
-            %   metadata - the meta data, a struct with fields:
-            %       keys      = the meta data keys (the names for the meta data)
-            %       values    = the meta data values (the values for the meta data)
-            %       hashtable = a Java Hashtable of keys and their values
-            
-            % Open the ND2 file.
-            data = bfopen(filename);
-            
-            % Extract the metadata.
-            hashtable = data{1,2};
-            keys = arrayfun(@char, hashtable.keySet.toArray, 'UniformOutput', false);
-            values = cellfun(@(x) hashtable.get(x), keys, 'UniformOutput', false);
-            
-            % Organize the metadata.
-            metadata.keys = keys;
-            metadata.values = values;
-            metadata.hashtable = hashtable;
-            
-            % Initialize the image volume information.
-            xPixelsI = find(contains(keys, 'Global uiWidth') ...
-                & ~contains(keys, 'Bytes'), 1);
-            yPixelsI = find(contains(keys, 'Global uiHeight') ...
-                & ~contains(keys, 'Bytes'), 1);
-            numZSlicesI = find(contains(keys, 'Global uiCount'), 1);
-            xyScaleI = find(contains(keys, 'Global dCalibration'), 1);
-            zScaleI = find(contains(keys, 'Global dZStep'), 1);
-            
-            % Extract the image volume information.
-            image.pixels = [ ...
-                values{xPixelsI}; ...
-                values{yPixelsI}; ...
-                values{numZSlicesI}];
-            image.scale = [ ...
-                values{xyScaleI}; ...
-                values{xyScaleI}; ...
-                values{zScaleI}];
-            
-            % Initialize the channel information.
-            numChannelsI = find(contains(keys, 'Global Number of Picture Planes'), 1);
-            channelsI = find(contains(keys, 'Global Name #'));
-            
-            % Extract the channel information.
-            numChannels = round(str2double(values{numChannelsI}));
-            channelKeys = keys(channelsI);
-            channelValues = values(channelsI);
-            image.channels = cell(numChannels,1);
-            for i = 1:numChannels
-                
-                % Get the channel name.
-                channelI = find(endsWith(channelKeys, num2str(i + 1)), 1);
-                image.channels{i} = channelValues{channelI};
-            end
-            
-            % Default to BGRW.
-            % Note: can't find color info in the metafile.
-            red = [1,0,0];
-            green = [0,1,0];
-            blue = [0,0,1];
-            white = [1,1,1];
-            image.colors = nan(numChannels,3);
-            image.colors(1,:) = blue;
-            image.colors(2,:) = green;
-            image.colors(3,:) = red;
-            for i = 4:numChannels
-                image.colors(i,:) = white;
-            end
-            image.dicChannel = nan;
-            
-            % Try using the channel names to determine their colors.
-            for i = 1:length(image.channels)
-                wavelength = sscanf(image.channels{i}, '%f');
-                
-                % DIC.
-                if isempty(wavelength) || isnan(wavelength) || wavelength < 350
-                    image.dicChannel = i;
-                    image.colors(i,:) = white;
-                elseif wavelength < 440
-                    image.colors(i,:) = blue;
-                elseif wavelength < 530
-                    image.colors(i,:) = green;
-                else
-                    image.colors(i,:) = red;
-                end
-            end
-            
-            % Organize the image volume.
-            imageData = data{1,1};
-            image.data = uint16(nan([image.pixels; numChannels]'));
-            for i=1:size(imageData,1)
-                
-                % Get the image plane data.
-                dataStrs = split(imageData{i,2}, ';');
-                zStr = strtrim(dataStrs{end-1});
-                cStr = strtrim(dataStrs{end});
-                
-                % Assemble the image.
-                z = sscanf(zStr,'Z=%f');
-                c = sscanf(cStr,'C=%f');
-                if isempty(z) || isnan(z)
-                    z = 1;
-                end
-                if isempty(c) || isnan(c)
-                    c = 1;
-                end
-                image.data(:,:,z,c) = imageData{i,1}';
-            end
         end
 
         function dimension_struct = get_dimensions(file)
@@ -378,11 +252,31 @@ classdef nd2
     end
 
     methods (Static, Access = private)
-        function data = get_data(file)
+        function write_data(np_file, nd2_file)
         end
 
-        function write_data(np_file, nd2_file)
+        function scale = parse_scale(file, pfx)
+            if ~exist('pfx', 'var')
+                xy_scale = Program.Handlers.channels.get_keys(file, key_map('xy_scale'), 'globals');
+                z_scale = Program.Handlers.channels.get_keys(file, key_map('z_scale'), 'globals');
 
+                if isempty(xy_scale) || isempty(z_scale)
+                    Program.Handlers.channels.parse_scale(file, 'Global ');
+                end
+
+            else
+                xy_scale = Program.Handlers.channels.get_keys(file, sprintf("%s %s", pfx, key_map('xy_scale')), 'globals');
+                z_scale = Program.Handlers.channels.get_keys(file,  sprintf("%s %s", pfx, key_map('xy_scale')), 'globals');
+
+            end
+
+            if isempty(xy_scale) || isempty(z_scale)
+                scale = [0 0 0];
+
+            else
+                scale = [xy_scale(1) xy_scale(2) z_scale];
+                
+            end
         end
 
         function obj = get_keys(file, query, scope)
