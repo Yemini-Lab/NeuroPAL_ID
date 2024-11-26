4acfc889f443b1e5a4631666852c9564860316d8
Kevin Rusch <SEP271905@protonmail.com>
2024-11-26 11:36:21 -0500

    bedant debug print fix

--- +Program/+Routines/+Debug/rotation.m
+++ +Program/+Routines/+Debug/rotation.m
@@ -6,6 +6,9 @@ function debug_struct = rotation(save_flag)
     debug_struct.graphics = Program.Routines.Debug.graphics();
 
     if nargin > 0
+        if ~isfolder('debug')
+            mkdir('debug');
+        end
         Program.Helpers.save_and_open(fullfile('debug', 'rotation_debug.mat'), debug_struct);
     end
 end
