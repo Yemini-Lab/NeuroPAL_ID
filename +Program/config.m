01a40873e0e3884ac5faeda483b80a87c9439171
Kevin Rusch <SEP271905@protonmail.com>
2024-11-26 10:03:31 -0500

    Create config.m

---
+++ +Program/config.m
@@ -0,0 +1,13 @@
+classdef config
+    %CONFIG Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Constant)
+        defaults = dictionary( ...
+            'class', {'uint16'});
+    end
+    
+    methods (Static)
+    end
+end
+
