2997a5476068521a1b5f9a0b861471262ca5378f
Kevin Rusch <SEP271905@protonmail.com>
2025-01-14 15:55:24 -0500

    fix dd_sync helper func to recognize self source

--- +Program/+Helpers/dd_sync.m
+++ +Program/+Helpers/dd_sync.m
@@ -1,8 +1,9 @@
-function dd_sync(old_value, new_value, group)
-    for dd=1:Program.Handlers.channels.config('max_channels')
+function dd_sync(source, old_value, new_value, group)
+    app = Program.app;
+    for dd=1:Program.Handlers.channels.config{'max_channels'}
         this_handle = sprintf(group, dd);
         this_dd = app.(this_handle);
-        if strcmp(new_value, this_dd.Value)
+        if strcmp(new_value, this_dd.Value) && this_dd ~= source
             this_dd.Value = old_value;
         end
     end
