f2e4b783fcdf268b85bc28068ba5547b99685e1c
Kevin Rusch (LabPC) <kwrusch@seas.upenn.edu>
2025-01-16 14:25:39 -0500

    Update agnostic_vol_check.m

--- +Program/+Validation/agnostic_vol_check.m
+++ +Program/+Validation/agnostic_vol_check.m
@@ -1,4 +1,4 @@
-function is_video = agnostic_vol_check)
+function is_video = agnostic_vol_check()
     app = Program.app;
     stack = dbstack;
     is_video = strcmpi(app.VolumeDropDown.Value, 'video') || contains(stack, 'TraceActivityMenu_2Selected');
