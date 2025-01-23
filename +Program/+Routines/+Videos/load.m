565a2c1b881b81d73390819cbd2601e943e14a9a
Kevin Rusch <SEP271905@protonmail.com>
2025-01-23 18:13:33 -0500

    Add cache check

--- +Program/+Routines/+Videos/load.m
+++ +Program/+Routines/+Videos/load.m
@@ -13,5 +13,18 @@ function load(file)
         case '.tif'
             app.load_tif()
     end
+
+    cache_path = strrep(path, ext, '_vt_cache.mat');
+    if exist(cache_path, "file") == 2
+        prompt_msg = sprintf("Found existing neuron track cache:\n%s\nLoad tracks from this cache?", cache_path);
+        check = uiconfirm(Program.window, ...
+            prompt_msg, "NeuroPAL_ID", ...
+            "Options", ["Yes", "No"]);
+
+        if strcmp(check, "Yes")
+            delete(path);
+            Program.Routines.Videos.tracks.load(cache_path);
+        end
+    end
 end
 
