5fa1da50bf0c0d00f42479a4aa57c8da25b407e4
Kevin Rusch <SEP271905@protonmail.com>
2025-02-11 16:03:14 -0500

    Update save.m

--- +Program/+Routines/+Processing/save.m
+++ +Program/+Routines/+Processing/save.m
@@ -1,38 +1,22 @@
-function save(specific_action)
+function save()
     app = Program.app;
 
     d = uiprogressdlg(Program.window, ...
         "Message", "Updating file...", ...
         "Indeterminate", "off");
 
-    if ~exist('specific_action', 'var')
-        switch app.VolumeDropDown.Value
-            case 'Colormap'
-                Methods.ChunkyMethods.apply_colormap(app, fieldnames(app.flags), d); 
-                fEvent.file = app.proc_image.Properties.Source;
-                app.OpenFile(fEvent);  
-            case 'Video'
-                Methods.ChunkyMethods.apply_video(app, fieldnames(app.flags), d);
-        end
+    switch app.VolumeDropDown.Value
+        case 'Colormap'
+            Methods.ChunkyMethods.apply_colormap(app, fieldnames(app.flags), d);
 
-        app.flags = struct();
-
-    else
-        switch app.VolumeDropDown.Value
-            case 'Colormap'
-                Methods.ChunkyMethods.apply_colormap(app, {specific_action}, d);
-                fEvent.file = app.proc_image.Properties.Source;
-                app.OpenFile(fEvent);  
-
-            case 'Video'
-                Methods.ChunkyMethods.apply_video(app, {specific_action}, d);
-        end
-
+        case 'Video'
+            Methods.ChunkyMethods.apply_video(app, fieldnames(app.flags), d);
     end
-    
+
+    app.flags = struct();
     close(d)
 
-    check = uiconfirm(Program.Window, ...
+    check = uiconfirm(Program.window, ...
         "Successfully updated file. Load into main tab?", "NeuroPAL_ID", ...
         "Options",["Yes", "No"]);
     if strcmp(check, "Yes")
