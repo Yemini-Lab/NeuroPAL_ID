46551d1131f1c1312ae8c26060a1eb2c1a2f5b7e
Kevin Rusch <SEP271905@protonmail.com>
2025-03-13 15:49:06 -0400

    Create viewing_video.m

---
+++ +Program/+GUI/viewing_video.m
@@ -0,0 +1,17 @@
+function bool = viewing_video()
+    state = Program.states;
+    source = dbstack();
+    source = source(2).name;
+
+    using_video_func = contains(source, 'video');
+    using_video_file = state.active_volume.is_video;
+    if ~isempty(state.interface)
+        using_video_tab = any(ismember(state.interface, ...
+            {'1', 'Video Tracking', 'track', 'tracking'}));
+    else
+        using_video_tab = 0;
+    end
+
+    bool = using_video_file || using_video_tab || using_video_func;
+end
+
