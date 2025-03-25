721c67920aab5d1bbb05169be1deb7fb0899b6c2
Kevin Rusch <SEP271905@protonmail.com>
2025-03-25 15:52:45 -0400

    propagate new apply call in guihandling

--- +Program/GUIHandling.m
+++ +Program/GUIHandling.m
@@ -565,7 +565,7 @@ classdef GUIHandling
         function proc_save_prompt(app, action)
             check = uiconfirm(app.CELL_ID, "Do you want to save this operation to the file?", "NeuroPAL_ID", "Options", ["Yes", "No, stick with preview"]);
             if strcmp(check, "Yes")
-                app.proc_apply_processing(action);
+                Program.Routines.Processing.apply(action);
                 if isfield(app.flags, action)
                     app.flags = rmfield(app.flags, action);
                 end
