89d1929afbe69387f543335c931eb8525e68c14f
Kevin Rusch <SEP271905@protonmail.com>
2025-04-14 12:28:21 -0400

    Revert "upgrade bfmatlab"
    This reverts commit c5e67194210788c37f4b5fc8c15a271f945d7f27.

--- External_Dependencies/bfmatlab/bfGetPlane.m
+++ External_Dependencies/bfmatlab/bfGetPlane.m
@@ -22,32 +22,24 @@ function I = bfGetPlane(r, varargin)
 
 % OME Bio-Formats package for reading and converting biological file formats.
 %
-% Copyright (C) 2012 - 2021 Open Microscopy Environment:
+% Copyright (C) 2012 - 2016 Open Microscopy Environment:
 %   - Board of Regents of the University of Wisconsin-Madison
 %   - Glencoe Software, Inc.
 %   - University of Dundee
 %
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are met:
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as
+% published by the Free Software Foundation, either version 2 of the
+% License, or (at your option) any later version.
 %
-% 1. Redistributions of source code must retain the above copyright
-%    notice, this list of conditions and the following disclaimer.
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
 %
-% 2. Redistributions in binary form must reproduce the above copyright
-%    notice, this list of conditions and the following disclaimer in
-%    the documentation and/or other materials provided with the distribution.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-% POSSIBILITY OF SUCH DAMAGE.
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 % Input check
 ip = inputParser;
@@ -57,19 +49,24 @@ ip.addRequired('r', isValidReader);
 ip.parse(r);
 
 % Plane check
-isValidPlane = @(p) bfTestInRange(p,'iPlane',r.getImageCount());
+isValidPlane = @(x) isscalar(x) && ismember(x, 1 : r.getImageCount());
 % Optional tile arguments check
-isValidX = @(x) bfTestInRange(x,'x',r.getSizeX());
-isValidY = @(y) bfTestInRange(y,'y',r.getSizeY());
-isValidWidth = @(w) bfTestInRange(w,'width',r.getSizeX()-varargin{2}+1);
-isValidHeight = @(h) bfTestInRange(h,'height',r.getSizeY()-varargin{3}+1);
-
+isValidX = @(x) isscalar(x) && ismember(x, 1 : r.getSizeX());
+isValidY = @(x) isscalar(x) && ismember(x, 1 : r.getSizeY());
 ip.addRequired('iPlane', isValidPlane);
 ip.addOptional('x', 1, isValidX);
 ip.addOptional('y', 1, isValidY);
-ip.addOptional('width', r.getSizeX(), isValidWidth);
-ip.addOptional('height', r.getSizeY(), isValidHeight);
+ip.addOptional('width', r.getSizeX(), isValidX);
+ip.addOptional('height', r.getSizeY(), isValidY);
 ip.parse(r, varargin{:});
+
+% Additional check for tile size
+assert(ip.Results.x - 1 + ip.Results.width <= r.getSizeX(),...
+     'MATLAB:InputParser:ArgumentFailedValidation',...
+     'Invalid tile size');
+assert(ip.Results.y - 1 + ip.Results.height <= r.getSizeY(),...
+     'MATLAB:InputParser:ArgumentFailedValidation',...
+     'Invalid tile size');
 
 % Get pixel type
 pixelType = r.getPixelType();
@@ -82,13 +79,22 @@ plane = r.openBytes(...
     ip.Results.iPlane - 1, ip.Results.x - 1, ip.Results.y - 1, ...
     ip.Results.width, ip.Results.height);
 
-% Convert byte array to MATLAB image
-I = javaMethod('makeDataArray2D', 'loci.common.DataTools', plane, ...
-    bpp, fp, little, ip.Results.height);
+% convert byte array to MATLAB image
+if sgn
+    % can get the data directly to a matrix
+    I = javaMethod('makeDataArray2D', 'loci.common.DataTools', plane, ...
+        bpp, fp, little, ip.Results.height);
+else
+    % get the data as a vector, either because makeDataArray2D
+    % is not available, or we need a vector for typecast
+    I = javaMethod('makeDataArray', 'loci.common.DataTools', plane, ...
+        bpp, fp, little);
+end
+
+% Java does not have explicitly unsigned data types;
+% hence, we must inform MATLAB when the data is unsigned
 if ~sgn
-    % Java does not have explicitly unsigned data types;
-    % hence, we must inform MATLAB when the data is unsigned
-    I = I(:);        % Need vector for typecast
+    % NB: arr will always be a vector here
     switch class(I)
         case 'int8'
             I = typecast(I, 'uint8');
@@ -99,5 +105,10 @@ if ~sgn
         case 'int64'
             I = typecast(I, 'uint64');
     end
-    I = reshape(I, [ip.Results.height ip.Results.width]); % Convert back to matrix
+end
+
+if isvector(I)
+    % convert results from vector to matrix
+    shape = [ip.Results.width ip.Results.height];
+    I = reshape(I, shape)';
 end
